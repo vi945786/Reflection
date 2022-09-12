67dc6c9a72822d71db190374e5c2561c51801329
TheOneAndOnlyDanSan <87494724+TheOneAndOnlyDanSan@users.noreply.github.com>
2022-09-12 23:16:14 +0300

    Add files via upload

---
+++ src/main/java/reflection/FieldReflection.java
@@ -0,0 +1,85 @@
+package reflection;
+
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static reflection.Utils.*;
+
+public class FieldReflection {
+
+    /**
+     * sets field's value
+     * @param f field to change
+     * @param instance instance to change the field in (null if field is static)
+     * @param value the value to change the field to
+     */
+    public static void setFieldValue(Field f, Object instance, Object value) {
+        forceAccessible(f);
+        forceSet(f, value, instance instanceof Class<?> ? null : instance);
+    }
+
+    /**
+     * gets field's value
+     * @param f field to get value of
+     * @param instance instance to get field value of
+     * @return value of the field in the instance (null if field is static)
+     */
+    public static Object getFieldValue(Field f, Object instance) {
+        try {
+            forceAccessible(f);
+            return f.get(instance);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * gets field from class and all superclasses
+     * @param clazz the class the field is in
+     * @param name the name of the field
+     * @return the field
+     */
+    public static Field getField(Class<?> clazz, String name) {
+            for(Field field : getFields(clazz, true)) {
+                if (field.getName().equals(name)) {
+                    return field;
+                }
+            }
+        return null;
+    }
+
+    /**
+     * gets all fields from class and all superclasses if specified
+     * @param clazz the class the field is in
+     * @param includeInheritedFields if to get fields from superclasses
+     * @return the fields specified
+     */
+    public static Field[] getFields(Class<?> clazz, boolean includeInheritedFields) {
+        try {
+            List<Field> fields = new ArrayList<>();
+
+            Method m = Class.class.getDeclaredMethod("getDeclaredFields0", boolean.class);
+            m.setAccessible(true);
+
+            while (clazz != null) {
+                for (Field field : (Field[]) m.invoke(clazz, false)) {
+
+                    Method copy = Field.class.getDeclaredMethod("copy");
+                    forceAccessible(copy);
+
+                    fields.add((Field) copy.invoke(field));
+                }
+                if(includeInheritedFields) {
+                    clazz = clazz.getSuperclass();
+                }
+            }
+
+            return fields.toArray(new Field[]{fields.get(0)});
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
